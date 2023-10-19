c8b569be675011d053b90130410ee5b5c70839a0
HuanzuoSP <18261112975@163.com>
2023-10-19 22:23:26 +0800

    update file

--- domainToClashYaml.py
+++ domainToClashYaml.py
@@ -6,18 +6,9 @@ import os
 script_dir = os.path.dirname(__file__)
 
 # 获取文件夹列表，只包括 'domain' 和 'domain-suffix' 文件夹
-folder_names = [folder for folder in os.listdir('.') if os.path.isdir(folder) and folder in ['domain', 'domain-suffix']]
-
-# 获取当前系统时间并格式化为字符串
-current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-for folder_name in folder_names:
+for folder_name in [folder for folder in os.listdir('.') if os.path.isdir(folder) and folder in ['domain', 'domain-suffix']]:
     # 构建 Clash 配置数据
     folder_path = os.path.join(script_dir, folder_name)
-
-    # 创建模板加载器和模板环境
-    template_loader = jinja2.FileSystemLoader(searchpath=[script_dir, os.path.join(script_dir, folder_name)])
-    env = jinja2.Environment(loader=template_loader)
 
     # 获取文件夹中的域名列表文件
     for filename in os.listdir(folder_path):
@@ -34,15 +25,15 @@ for folder_name in folder_names:
                 "name": target_filename,
                 "author": "HuanzuoSP",
                 "repo": "https://github.com/HuanzuoSP/proxyrules/" + folder_name + "/" + target_filename,
-                "updated": current_time,
+                "updated": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                 "total_domains": len(domains),
                 "domains": domains,
             }
 
-            # 渲染模板并写入 Clash 配置文件
+            # 渲染模板并写入 .yaml 配置文件
             with open(os.path.join(folder_path, target_filename), 'w') as yaml_file:
-                yaml_file.write(env.get_template(f"{folder_name}.j2").render(clash_config))
-
+                yaml_file.write(jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=[script_dir, os.path.join(script_dir, folder_name)])).get_template(f"{folder_name}.j2").render(clash_config))
+            # 写入 .list 配置文件
             with open(os.path.join(folder_path, filename.replace(".txt", ".list")), 'w') as list_file:
                 for domain in domains:
                     if folder_name == "domain":
